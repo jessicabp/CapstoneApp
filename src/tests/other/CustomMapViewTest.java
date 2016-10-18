e131cc830980d6b08bdf094e481b8b481e213ac1
Umesh Ravji <u.ravji@hotmail.com>
2016-10-18 22:59:36 +1300

    CustomMapViewTest added.

--- src/tests/other/CustomMapViewTest.java
+++ src/tests/other/CustomMapViewTest.java
@@ -6,5 +6,18 @@ import org.junit.Test;
 
 public class CustomMapViewTest {
 
-    // TODO: Umesh please write test for CustomMapView
+    @Test
+    public void defaultConstructorTest() {
+        CustomMapView customMapView = new CustomMapView();
+        Assert.assertEquals(280.0, customMapView.getMaxHeight(), 0);
+        Assert.assertEquals(Double.MAX_VALUE, customMapView.getMaxWidth(), 0);
+    }
+
+    @Test
+    public void maxHeightConstructorTest() {
+        CustomMapView customMapView = new CustomMapView(100.0);
+        Assert.assertEquals(100.0, customMapView.getMaxHeight(), 0);
+        Assert.assertEquals(Double.MAX_VALUE, customMapView.getMaxWidth(), 0);
+    }
+
 }
